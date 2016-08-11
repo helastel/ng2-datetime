fb099ec66dc74cda01a077e959cbe6072c0920e7
Mike Dodd <mick.dodd@gmail.com>
2016-08-11 15:02:10 +0100

    new

--- src/ng2-datetime/ng2-datetime.ts
+++ src/ng2-datetime/ng2-datetime.ts
@@ -1,26 +1,23 @@
 import {Component, Output, Input, EventEmitter, HostListener, AfterViewInit, OnDestroy} from '@angular/core';
 import {ControlValueAccessor, NgControl} from '@angular/common';
 import MaskedInput from 'angular2-text-mask';
-
-
 @Component({
     selector: 'datetime',
     directives: [MaskedInput],
     template: `
     <div class="form-inline">
         <div id="{{idDatePicker}}" class="input-group date">
-            <input (keyup)="question.setValue($event)" [placeholder]="datePlaceholderString" type="text" class="form-control"/>
+            <input [attr.id]="question.key" [attr.aria-labelledby]="'label-' + question.key" (keyup)="question.setValue($event)" [placeholder]="datePlaceholderString" type="text" class="form-control"/>
             <div class="input-group-addon">
                 <span [ngClass]="datepickerOptions.icon || 'glyphicon glyphicon-th'"></span>
             </div>
         </div>
         <div class="input-group bootstrap-timepicker timepicker">
-            <input  (keyup)="question.setValue($event)" id="{{idTimePicker}}" [placeholder]="timePlaceholderString" type="text" class="form-control input-small">
+            <input [attr.id]="question.key+'-time'" [attr.aria-labelledby]="'label-' + question.key" (keyup)="question.setValue($event)" id="{{idTimePicker}}" [placeholder]="timePlaceholderString" type="text" class="form-control input-small">
             <span class="input-group-addon"><i [ngClass]="timepickerOptions.icon || 'glyphicon glyphicon-time'"></i></span>
         </div>
     </div>
    `
-
 })
 export class NKDatetime implements ControlValueAccessor, AfterViewInit, OnDestroy {
     @Output()
@@ -39,34 +36,26 @@ export class NKDatetime implements ControlValueAccessor, AfterViewInit, OnDestro
     datePlaceholderString: string;
     //    @Input('question')
     @Input() question: any;
-
-
     date: Date; // ngModel
     // instances
     datepicker: any;
     timepicker: any;
-
     private idDatePicker: string = uniqueId('q-datepicker_');
     private idTimePicker: string = uniqueId('q-timepicker_');
-
     @HostListener('dateChange', ['$event'])
     onChange = (_) => { };
     onTouched = () => { };
-
     constructor(ngControl: NgControl) {
         ngControl.valueAccessor = this; // override valueAccessor
     }
-
     ngAfterViewInit() {
         this.init();
     }
-
     ngOnDestroy() {
         if (this.datepicker) {
 //            this.datepicker.destroy();
         }
     }
-
     writeValue(value: any): void {
         this.date = value;
         if (isDate(this.date)) {
@@ -75,24 +64,19 @@ export class NKDatetime implements ControlValueAccessor, AfterViewInit, OnDestro
             }, 0);
         }
     }
-
     registerOnChange(fn: (_: any) => void): void {
         this.onChange = fn;
     }
-
     registerOnTouched(fn: () => void): void {
         this.onTouched = fn;
     }
-
     //////////////////////////////////
-
     private init() {
         if (!this.datepicker && this.datepickerOptions !== false) {
             this.datepicker = (<any>$('#' + this.idDatePicker)).datepicker(this.datepickerOptions);
             this.datepicker
                 .on('changeDate', (e) => {
                     let newDate: Date = e.date;
-
                     if (isDate(this.date) && isDate(newDate)) {
                         // get hours/minutes
                         var h = this.date.getHours();
@@ -100,14 +84,12 @@ export class NKDatetime implements ControlValueAccessor, AfterViewInit, OnDestro
                         newDate.setHours(h);
                         newDate.setMinutes(m);
                     }
-
                     this.date = newDate;
                     this.dateChange.emit(newDate);
                 });
         } else if (this.datepickerOptions === false) {
             (<any>$('#' + this.idDatePicker)).remove();
         }
-
         if (!this.timepicker && this.timepickerOptions !== false) {
             let options = jQuery.extend({ defaultTime: false }, this.timepickerOptions);
             this.timepicker = (<any>$('#' + this.idTimePicker)).timepicker(options);
@@ -127,7 +109,6 @@ export class NKDatetime implements ControlValueAccessor, AfterViewInit, OnDestro
                     }
                     if (!isDate(this.date)) {
                         this.date = new Date();
-
                         if (this.datepicker !== undefined) {
                             this.datepicker.datepicker('update', this.date.toLocaleDateString('en-US'));
                         }
@@ -140,15 +121,13 @@ export class NKDatetime implements ControlValueAccessor, AfterViewInit, OnDestro
             (<any>$('#' + this.idTimePicker)).parent().remove();
         }
     }
-
     private updateModel(date?: Date) {
-        
+
         console.log("DATE UPDATE");
         // update date
         if (this.datepicker !== undefined) {
             this.datepicker.datepicker('update', date.toLocaleDateString('en-US'));
         }
-
         // update time
         if (this.timepicker !== undefined) {
             let hours = this.date.getHours();
@@ -157,21 +136,17 @@ export class NKDatetime implements ControlValueAccessor, AfterViewInit, OnDestro
                 hours = (hours === 0 || hours === 12) ? 12 : hours % 12;
             }
             let meridian = this.date.getHours() >= 12 ? ' PM' : ' AM';
-
             this.timepicker.timepicker('setTime', this.pad(hours) + ':' + this.date.getMinutes() + meridian);
         }
     }
-
     private pad(value: any) {
         return (value && value.toString().length < 2) ? '0' + value : value.toString();
     }
 }
-
 let id: number = 0;
 function uniqueId(prefix: string): string {
     return prefix + ++id;
 }
-
 function isDate(obj) {
     return Object.prototype.toString.call(obj) === '[object Date]';
 }

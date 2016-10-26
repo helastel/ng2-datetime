f2e7b3eef2e672df831e67f08499102894ea7d36
mikedodd <mick.dodd@gmail.com>
2016-10-26 17:43:45 +0100

    Update ng2-datetime.js

--- src/ng2-datetime/ng2-datetime.js
+++ src/ng2-datetime/ng2-datetime.js
@@ -64,7 +64,7 @@ var NKDatetime = (function () {
                 _this.date = newDate;
                 _this.dateChange.emit(newDate);
                 _this.question.setValue(_this.date);
-                _this.updateModel(_this.date);
+               // _this.updateModel(_this.date);
             });
         }
         else if (this.datepickerOptions === false) {
@@ -96,7 +96,7 @@ var NKDatetime = (function () {
                 _this.date.setHours(parseInt(hours));
                 _this.date.setMinutes(e.time.minutes);
                 _this.dateChange.emit(_this.date);
-                _this.question.setValue(_this.date);
+              //  _this.question.setValue(_this.date);
             });
         }
         else if (this.timepickerOptions === false) {
